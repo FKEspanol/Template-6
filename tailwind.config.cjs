790514c2c75e6e06e749e152824882e0eb41400a
karlo <karlsgit1126@gmail.com>
2023-06-08 20:20:50 +0800

    done with getstarted section in home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,15 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-
       colors: {
         dark: "#1f1f1f",
         primary: "#0078d4"
       },
+
+      backgroundColor: {
+        dark: "#1f1f1f",
+        primary: "#0078d4"
+      }
     },
   },
   plugins: [],
