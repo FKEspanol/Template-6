74ea8fda3d57265423b4630089b361ff33173288
Francis Karlo Espanol <112460235+FKEspanol@users.noreply.github.com>
2023-06-09 10:04:14 +0800

    done with construction section in home page

---
+++ src/dataContents/construction.ts
@@ -0,0 +1,33 @@
+interface ConstructionData {
+    imgUrl: string;
+    cardTitle: string;
+    cardContent: string;
+}
+const data: ConstructionData[] = [
+    {
+        imgUrl: "bg-[url('/assets/images/constructions-1.jpg')]",
+        cardTitle: "Eligendi omnis sunt veritatis.",
+        cardContent:
+            "Fuga in dolorum et iste et culpa. Commodi possimus nesciunt modi voluptatem placeat deleniti adipisci. Cum delectus doloribus non veritatis. Officia temporibus illo magnam. Dolor eos et.",
+    },
+    {
+        imgUrl: "bg-[url('/assets/images/constructions-2.jpg')]",
+        cardTitle: "Possimus ut sed velit assumenda",
+        cardContent:
+            "Sunt deserunt maiores voluptatem autem est rerum perferendis rerum blanditiis. Est laboriosam qui iste numquam laboriosam voluptatem architecto. Est laudantium sunt at quas aut hic. Eum dignissimos.",
+    },
+    {
+        imgUrl: "bg-[url('/assets/images/constructions-3.jpg')]",
+        cardTitle: "Error beatae dolor inventore aut",
+        cardContent:
+            "Dicta porro nobis. Velit cum in. Nesciunt dignissimos enim molestiae facilis numquam quae quaerat ipsam omnis. Neque debitis ipsum at architecto officia laboriosam odit. Ut sunt temporibus nulla culpa.",
+    },
+    {
+        imgUrl: "bg-[url('/assets/images/constructions-4.jpg')]",
+        cardTitle: "Expedita voluptas ut ut nesciunt",
+        cardContent:
+            "Aut est quidem doloremque voluptatem magnam quis excepturi vero quia. Eum eos doloremque architecto illo at beatae dolore. Fugiat suscipit et sint ratione dolores. Aut aliquid ea dolores libero nobis.",
+    },
+];
+
+export default data;
