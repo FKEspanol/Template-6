be1971d8245c6852b8c898295e1f89749c294ded
Francis Karlo Espanol <112460235+FKEspanol@users.noreply.github.com>
2023-06-09 14:47:26 +0800

    done with services section in the home page

---
+++ src/dataContents/services.ts
@@ -0,0 +1,46 @@
+interface Card {
+    cardIcon: string;
+    cardTitle: string;
+    cardContent: string;
+}
+
+const data: Card[] = [
+    {
+        cardIcon: "fluent:city-24-filled",
+        cardTitle: "Nesciunt Mete",
+        cardContent:
+            "Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.",
+    },
+    {
+        cardIcon: "ic:outline-construction",
+        cardTitle: "Eosle Commodi",
+        cardContent:
+            "Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.",
+    },
+    {
+        cardIcon: "icon-park-outline:compass",
+        cardTitle: "Ledo Markt",
+        cardContent:
+            "Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.",
+    },
+    {
+        cardIcon: "uil:trowel",
+        cardTitle: "Asperiores Commodit",
+        cardContent:
+            "Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error ea fuga sit provident adipisci neque.",
+    },
+    {
+        cardIcon: "icon-park-outline:helmet-one",
+        cardTitle: "Velit Doloremque",
+        cardContent:
+            "Cumque et suscipit saepe. Est maiores autem enim facilis ut aut ipsam corporis aut. Sed animi at autem alias eius labore.",
+    },
+    {
+        cardIcon: "mdi:crane",
+        cardTitle: "Dolori Architecto",
+        cardContent:
+            "Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et debitis iure. Corrupti recusandae ducimus enim.",
+    },
+];
+
+export default data;
