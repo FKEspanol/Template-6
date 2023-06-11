c58bc23eee6612591b8e851643fd0103393151b0
Francis Karlo Espanol <112460235+FKEspanol@users.noreply.github.com>
2023-06-11 21:06:45 +0800

    added a footer section

---
+++ src/dataContents/footer.ts
@@ -0,0 +1,43 @@
+interface Links {
+   title: string;
+   links: string[];
+}
+
+const footerLinks: Links[] = [
+   {
+      title: "Useful Links",
+      links: [
+         "Home",
+         "About us",
+         "Services",
+         "Terms of service",
+         "Privacy policy",
+      ],
+   },
+   {
+      title: "Our Services",
+      links: [
+         "Web Design",
+         "Web Development",
+         "Product Mangement",
+         "Marketing",
+         "Graphic Design",
+      ],
+   },
+   {
+      title: "Hic solutasetp",
+      links: [
+         "Molestiae accusamus iure",
+         "Excepturi dignissimos",
+         "Suscipit distinctio",
+         "Dilecta",
+         "Sit quas consectetur",
+      ],
+   },
+   {
+      title: "Nobis illum",
+      links: ["Ipsam", "Laudantium dolorum", "Dinera", "Trodelas", "Flexo"],
+   },
+];
+
+export default footerLinks;
