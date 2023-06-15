67b514fd682be790e6214b28eca31e5899fe0f18
Francis Karlo Espanol <112460235+FKEspanol@users.noreply.github.com>
2023-06-15 09:36:28 +0800

    move atlservices data to separate file in dataContents folder

---
+++ src/dataContents/atlservices.ts
@@ -0,0 +1,34 @@
+interface SectionItems {
+    icon: string;
+    heading: string;
+    content: string;
+}
+
+const data: SectionItems[] = [
+    {
+        icon: "bi:easel",
+        heading: "Lorem Ipsum",
+        content:
+            "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
+    },
+    {
+        icon: "bi:patch-check",
+        heading: "Nemo Enim",
+        content:
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque",
+    },
+    {
+        icon: "uil:brightness",
+        heading: "Dine Pad",
+        content:
+            "Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis",
+    },
+    {
+        icon: "uil:brightness",
+        heading: "Tride clov",
+        content:
+            "Est voluptatem labore deleniti quis a delectus et. Saepe dolorem libero sit non aspernatur odit amet. Et eligendi",
+    },
+];
+
+export default data;
