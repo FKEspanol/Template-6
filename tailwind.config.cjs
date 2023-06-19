ebfd0925e0ef7080ca2695a513d0042a88ca262e
Francis Karlo Espanol <112460235+FKEspanol@users.noreply.github.com>
2023-06-19 11:08:53 +0800

    override breakpoints into a bootstrap breakpoint measurement

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      screens: {
+        'sm': '576px',
+        'md': '768px',
+        'lg': '992px',
+        'xl': '1200px',
+        'xxl': '1400px',
+      },
       colors: {
         dark: "#1f1f1f",
         primary: "#feb900",
